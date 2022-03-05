2a76c99f0907277b6e2b933560bdc7a180891027
Hai Duong <dohaiduong1803@gmail.com>
2022-03-05 09:54:54 +0700

    Update main.cpp

--- Binary Tree/main.cpp
+++ Binary Tree/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include "Tree.h"
+using namespace std;
 
+
+int main()
+{
+    Tree q;
+    cout <<"Pre-order" << endl;
+    q.preorder(q.root);
+    cout <<"In-order" << endl;
+    q.inOrder(q.root);
+    cout <<"Post-order" << endl;
+    q.postOrder(q.root);
+
+    return 0;
+}
